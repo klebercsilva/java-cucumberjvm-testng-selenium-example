b383d140c684bfd09ea8fb062ef3b1f7b72373e2
Kleber Silva <mxkleber@gmail.com>
2020-01-20 08:15:53 -0600

    fix StringUtils class now provides static methods to validate strings rather than being a singleton

--- src/test/java/util/StringUtils.java
+++ src/test/java/util/StringUtils.java
@@ -1,21 +1,12 @@
 package util;
 
 public class StringUtils {
-
-	private static StringUtils instance = null;
-	
-	private StringUtils() {}
-	
-	public static StringUtils getInstance() {
-		if(instance == null) {
-			instance = new StringUtils();
-		} 
-		return instance;
-	}
 	
 	public static boolean isValidEmail(String email) {
-		//TODO needs use regex to validate more accurately 
-		boolean isValid = email.contains("@") ? true : false;
-		return isValid;
+		return email.contains("@");
+	}
+
+	public static boolean isValidPhoneNumber(String phoneNumber) {
+		return phoneNumber.length() == 10;
 	}
 }
