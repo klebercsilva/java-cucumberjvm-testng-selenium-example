28c487973ebcac210f57b4642f80d880f187f44f
Kleber Silva <mxkleber@gmail.com>
2020-01-20 07:38:59 -0600

    feat use wrapper methods instead of the native impl

--- src/test/java/core/GenericAction.java
+++ src/test/java/core/GenericAction.java
@@ -1,23 +1,47 @@
 package core;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.testng.Assert.assertTrue;
 
 public class GenericAction implements IGenericAction {
 
-    private GenericAction() {}
+    private WebDriver driver;
 
-    @Override
-    public void click() {
+    public GenericAction(WebDriver driver) {
+        this.driver = driver;
     }
 
     @Override
-    public void enterText(By by) {
-
+    public void click(WebElement element) {
+        waitForElementVisible(element);
+        element.click();
     }
 
     @Override
-    public void waitForElementVisible() {
-
+    public void enterText(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
     }
 
+    @Override
+    public void assertDisplayed(WebElement element) {
+        assertTrue(element.isDisplayed());
+    }
+
+    @Override
+    public void chooseValue(By by, String value) {
+        new Select(driver.findElement(by)).selectByVisibleText(value);
+    }
+
+    @Override
+    public void waitForElementVisible(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
 }
--- src/test/java/core/IGenericAction.java
+++ src/test/java/core/IGenericAction.java
@@ -1,9 +1,12 @@
 package core;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public interface IGenericAction {
-    void click();
-    void enterText(By by);
-    void waitForElementVisible();
+    void click(WebElement element);
+    void chooseValue(By by, String value);
+    void assertDisplayed(WebElement element);
+    void enterText(WebElement element, String text);
+    void waitForElementVisible(WebElement element);
 }
